82075ee015a78daaaa8f3e9c0f5b7b2173ad7c19
sketchydane <99187184+sketchydanee@users.noreply.github.com>
2024-09-04 13:58:54 +0800

    Created Bluetooth Instance and Test Output

--- ESP32Display-EnvironmentMonitor/ESP32Display-EnvironmentMonitor.ino
+++ ESP32Display-EnvironmentMonitor/ESP32Display-EnvironmentMonitor.ino
@@ -265,6 +265,9 @@ void loop() {
   else
     sumRH = RH;
 
+  SerialBT.print("75|8,7,6,5,4,3,2,1");
+  delay(1000);
+  SerialBT.print("125|1,2,3,4,5,6,7,8");
   delay(1000);
 
   // Update every minute
