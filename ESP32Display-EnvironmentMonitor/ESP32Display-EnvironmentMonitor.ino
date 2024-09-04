271a0ea872d60320da2525dbfba65eda805416a9
JaedenCastro <b2025jmgcastro@pshs.edu.ph>
2024-09-04 21:10:51 +0800

    Cleaned up the code and removed artifacts from stat histogram

--- ESP32Display-EnvironmentMonitor/ESP32Display-EnvironmentMonitor.ino
+++ ESP32Display-EnvironmentMonitor/ESP32Display-EnvironmentMonitor.ino
@@ -3,20 +3,13 @@
 #include <TFT_eSPI.h>
 #include "FS.h"
 #include "SPIFFS.h"
-//#include <Adafruit_Sensor.h>
-//#include <DHT.h>
-//#include <DHT_U.h>
 #include "TableHeap.h"
 #include "Free_Fonts.h"
 #include "BluetoothSerial.h"
 
 // Pins
-const byte LEDB = 17;
-const byte LEDG = 16;
-const byte LEDR = 4;
 const byte LCD_BL_PIN = 27;
 const byte LDR_PIN = 34;
-//const byte DHT_PIN = 21;
 
 // Heat index constants from https://en.wikipedia.org/wiki/Heat_index
 const float c1 = -8.78469475556;
@@ -30,7 +23,6 @@ const float c8 = 7.2546E-4;
 const float c9 = -3.582E-6;
 
 TFT_eSPI tft = TFT_eSPI();
-//DHT_Unified dht(DHT_PIN, DHT11);
 BluetoothSerial SerialBT;
 
 #define TFT_GREY 0x2104 // Dark grey 16-bit colour
@@ -40,62 +32,13 @@ bool range_error = false;
 uint16_t backcolor = TFT_BLACK;
 
 const byte MAX_DATA = 195;
-int8_t dataRH[MAX_DATA];
 int8_t dataThi[MAX_DATA];
+
 byte counter = 0;
 float sumThi = -40.0;
 float sumRH = -40.0;
 
-void clearDataPoints() {
-  for (uint8_t i = 0; i < MAX_DATA; i++) {
-    dataRH[i] = 0;
-    dataThi[i] = 0;
-  }
-}
 
-void saveDataPoints(float RH, float Thi) {
-  // Shift arrays by one towards index 0
-  for (uint8_t i = 1; i < MAX_DATA; i++) {
-    dataRH[i - 1] = dataRH[i];
-    dataThi[i - 1] = dataThi[i];
-  }
-
-  dataRH[MAX_DATA - 1] = (uint8_t) RH;
-  dataThi[MAX_DATA - 1] = constrain(map((uint16_t) Thi * 10, 150, 550, 0, 100), 0, 100);    // From 15.0 to 55.0 deg C
-}
-/*
-void drawStatHistogramFramework() {
-  tft.drawFastHLine(90, 217, 480, TFT_WHITE);
-  tft.drawFastHLine(90, 319, 480, TFT_WHITE);
-
-  // Draw 15-minute lines
-  tft.setTextDatum(BC_DATUM);
-
-  // Draw time codes on x-axis
-  for (uint16_t i = 13; i > 0; i--) {
-    tft.drawNumber(i * 15, 480 - i * 30, 216, 2);
-  }
-
-  tft.setFreeFont(FSS12);
-  tft.setTextDatum(TL_DATUM);
-}
-
-void drawStatHistogram() {
-  for (uint8_t i = 0; i < MAX_DATA; i++) {
-    //tft.fillRect(x,y)
-    tft.fillRect(45 + i * 2, 218, 2, 318 - dataThi[i], TFT_BLACK);
-    tft.fillRect(45 + i * 2, 318 - dataThi[i], 2, 318, temperatureColors2(dataThi[i]));
-    tft.drawFastHLine(90 + i * 2, 318 - dataRH[i], 2, TFT_BLUE);
-  }
-
-  // Draw 15-minute lines
-  for (uint16_t i = 90; i < 480; i += 30)
-    tft.drawFastVLine(i, 218, 100, TFT_CYAN);
-
-  // Draw warning line at 40 degrees
-  tft.drawFastHLine(90, 256, 480, TFT_YELLOW);
-}
-*/
 int temperatureColors(int temp) {
   if (temp > 52)
     return 0xF800;      // Red
@@ -122,6 +65,7 @@ int temperatureColors2(int temp) {
     return 0x07E0;      // Green
 }
 
+/*
 int humidityColors(int RH) {
   if (RH > 60)
     return 0xF81F;      // Purple
@@ -130,8 +74,8 @@ int humidityColors(int RH) {
   else
     return 0xF800;      // Red
 }
-
-/*
+*/
+/* #I'm not gonna remove this yet, in case we do touchscreen at all
 void calibrateTouchscreen(bool recalibrate = false) {
   uint16_t calibrationData[5];
   bool valid = false;
@@ -186,28 +130,6 @@ void calibrateTouchscreen(bool recalibrate = false) {
 }*/
 
 void setup() {
-  // Configure and turn off the RGB LED
-  pinMode(LEDR, OUTPUT);
-  pinMode(LEDG, OUTPUT);
-  pinMode(LEDB, OUTPUT);
-
-  digitalWrite(LEDR, HIGH);
-  digitalWrite(LEDG, HIGH);
-  digitalWrite(LEDB, HIGH);
-
-  // Configure backlight
-  pinMode(LCD_BL_PIN, OUTPUT);
-  //ledcSetup(0, 5000, 8);
-  //ledcAttachPin(LCD_BL_PIN, 0);
-  //ledcWrite(0, 128);                // Turn on the backlight
-  digitalWrite(LCD_BL_PIN, HIGH);
-
-  // Configure the LDR
-  pinMode(LDR_PIN, ANALOG);
-  analogReadResolution(12);         // 12 bits
-  analogSetWidth(12);               // 12 bits
-  analogSetPinAttenuation(LDR_PIN, ADC_6db);   // Maximum voltage of 1350mV
-
   // Initialize LCD
   tft.begin();
   tft.invertDisplay(0);
@@ -222,7 +144,6 @@ void setup() {
   // Initialize sensor
   //dht.begin();
   drawTable();
-  clearDataPoints();
 
   Serial.begin(115200);
   SerialBT.begin("AsthmAlert"); //Bluetooth device name
@@ -231,22 +152,6 @@ void setup() {
 }
 
 void loop() {
-  // Adjust backlight from LDR readings
-  //ledcWrite(0, constrain(map(analogRead(LDR_PIN), 0, 800, 255, 5), 5, 255));
-
-  // Get DHT11 sensor values
-  //sensors_event_t event;
-
- //dht.temperature().getEvent(&event);
-  float temp = 3;
-  float temp2 = temp * temp;
-
-  //dht.humidity().(&event);
-  float RH = 2;
-  float RH2 = RH * RH;
-
-  // Compute heat index temperature equivalent
-  float Thi = c1 + c2 * temp + c3 * RH + c4 * temp * RH + c5 * temp2 + c6 * RH2 + c7 * temp2 * RH + c8 * temp * RH2 + c9 * temp2 * RH2;
   
   // Draw UI elements
   // drawRingMeter(Thi, xval, yval, x, y, r, "*AQI", temperatureColors(Thi), TFT_GREY, TFT_WHITE, TFT_BLACK);
@@ -274,6 +179,7 @@ const int cellWidth = 100; //wide
 const int cellHeight = 75; // tall
 const int startX = 10;
 const int startY = 10;
+// Temp, Humidity, VOC Index, PM (any)
 String elements[4][2] = {{"Temp", "Val1"}, {"Humidity", "Val2"}, {"VOC", "Val3"}, {"PM", "Val4"}};
 void drawTable() {
   tft.setTextSize(1);
@@ -299,8 +205,7 @@ void drawTable() {
       tft.setCursor(x, y);
       tft.setTextColor(TFT_WHITE);
       tft.print(elements[row][col]);
-       // Example text: "R1C1", "R1C2", etc. // this is the line that is the issue
-      // Temp, Humidity, VOC Index, PM (any)
+
     }
   }
   tft.setTextSize(2); 
